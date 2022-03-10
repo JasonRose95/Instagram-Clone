4299c90958351c969e83dddb9dea3837f6b5b12e
Jason Rosenthal <jasonrosenthalr@gmail.com>
2022-03-09 16:50:42 -0800

    Progress Profile View

---
+++ InstagramClone/InstagramClone/API/AuthService.swift
@@ -0,0 +1,50 @@
+//
+//  AuthService.swift
+//  InstagramClone
+//
+//  Created by Jason Rosenthal on 3/7/22.
+//
+
+import UIKit
+import Firebase
+import AVFoundation
+import FirebaseFirestore
+
+struct AuthCredentials {
+    let email: String
+    let password: String
+    let fullname: String
+    let username: String
+    let profileImage: UIImage
+}
+
+struct AuthService {
+    
+    static func logUserIn(withEmail email: String, password: String, completion: AuthDataResultCallback?) {
+        Auth.auth().signIn(withEmail: email, password: password, completion: completion)
+    }
+    
+    static func registerUser(withCredentials credentials: AuthCredentials, completion: @escaping(Error?) -> Void){
+       
+        ImageUploader.uploadImage(image: credentials.profileImage) { imageUrl in
+            Auth.auth().createUser(withEmail: credentials.email, password: credentials.password) { result, error in
+                
+                if let error = error {
+                    print("DEBUG: Failed to register user \(error.localizedDescription)")
+                          return
+                }
+                
+                guard let uid = result?.user.uid else { return }
+                
+                let data: [String: Any] = ["email": credentials.email,
+                                           "fullname": credentials.fullname,
+                                           "profileImageUrl": imageUrl,
+                                           "uid": uid,
+                                           "username": credentials.username]
+                
+                Firestore.firestore().collection("users").document(uid).setData(data, completion: completion)
+            }
+        }
+        
+    }
+}
--- InstagramClone/InstagramClone/Controller/FeedController.swift
+++ InstagramClone/InstagramClone/Controller/FeedController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 private let reuseIdentifier = "Cell"
 
@@ -19,12 +20,32 @@ class FeedController: UICollectionViewController {
         configureUI()
     }
     
+    //MARK: - Actions
+    
+    @objc func handleLogout() {
+        do {
+            try Auth.auth().signOut()
+            let controller = LoginController()
+            let nav = UINavigationController(rootViewController: controller)
+            nav.modalPresentationStyle = .fullScreen
+            self.present(nav, animated: true, completion: nil)
+        } catch {
+            print("Failed to sign out")
+        }
+    }
+    
     //MARK: - Helpers
     
     func configureUI(){
         collectionView.backgroundColor = .white
         
         collectionView.register(FeedCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout",
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(handleLogout))
+        navigationItem.title = "Feed"
     }
 }
 
@@ -33,7 +54,7 @@ class FeedController: UICollectionViewController {
 extension FeedController {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        return 10
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
