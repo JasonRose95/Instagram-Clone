f4c68e8b841858a4c788a0c6f80e17e133249085
Jason Rosenthal <jasonrosenthalr@gmail.com>
2022-03-24 10:57:37 -0700

    Upload comments to Firebase

---
+++ InstagramClone/InstagramClone/API/CommentService.swift
@@ -0,0 +1,22 @@
+//
+//  CommentService.swift
+//  InstagramClone
+//
+//  Created by Jason Rosenthal on 3/23/22.
+//
+
+import Firebase
+
+struct CommentService {
+    
+    static func uploadComment(comment: String, postID: String, user: User, completion: @escaping(FirestoreCompletion)) {
+        
+        let data: [String: Any] = ["uid": user.uid, "comment": comment, "timestamp": Timestamp(date: Date()), "username": user.username, "profileImageUrl": user.profileImageUrl]
+        
+        COLLECTION_POSTS.document(postID).collection("comments").addDocument(data: data, completion: completion)
+    }
+    
+    static func fetchComments() {
+        
+    }
+}
